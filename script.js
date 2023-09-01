d8f6d71632cac75c57e8a8b68c44a67ebd24755a
Ramiro Garces <ramgarces24@gmail.com>
2023-09-01 16:03:17 -0500

    Remove code from event listeners to refactor how variables are stored

--- script.js
+++ script.js
@@ -2,57 +2,44 @@
 let firstNum = "";
 let nextNum = "";
 let operator = "";
-let operatorFlag = false;
 let displayValue = '';
-
 
 const numberButtons = document.querySelectorAll(".numBtn");
 const operatorButtons = document.querySelectorAll(".opBtn");
 const equalsButton = document.querySelector(".equals-button");
 const display = document.querySelector(".display")
 
-
 numberButtons.forEach(btn => {
     btn.addEventListener('click', () => {
         console.log(`Pressed ${btn.innerHTML}`)
-        operatorFlag = false;
-        if (firstNum && operator) {
-            displayValue = "";
-        }
-        displayValue += btn.innerHTML;
-        display.innerHTML = displayValue;
+        
         console.log(`Display value: ${displayValue}`)
         console.log(`First number: ${firstNum}`)
         console.log(`Operator: ${operator}`)
+        console.log(`Array: ${valueArray}`)
     })
 })
 
 operatorButtons.forEach(btn => {
     btn.addEventListener('click', () => {
         console.log(`Pressed ${btn.innerHTML}`)
-        operatorFlag = true;
-        firstNum = displayValue;
-        displayValue = "";
-        displayValue += btn.innerHTML;
-        operator = displayValue;
-        display.innerHTML = "";
+        
         console.log(`Display value: ${displayValue}`)
-        console.log(`Op flag: ${operatorFlag}`)
         console.log(`First number: ${firstNum}`)
         console.log(`Operator: ${operator}`)
+        console.log(`Array: ${valueArray}`)
     })
 })
 
-
 equalsButton.addEventListener('click', () => {
     console.log(`Pressed ${equalsButton.innerHTML}`)
-    nextNum = displayValue;
+    
     console.log(`Display value: ${displayValue}`)
     console.log(`First number: ${firstNum}`)
     console.log(`Next number: ${nextNum}`)
     console.log(`Operator: ${operator}`)
+    console.log(`Array: ${valueArray}`)
 })
-
 
 // Functions
 function operate() {
