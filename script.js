8319994e5655e67f39f788dcc5fdafa4b291f1a6
Ramiro Garces <ramgarces24@gmail.com>
2023-09-06 10:09:39 -0500

    Separate event listeners for number and operator buttons

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let firstNum = '';
 let nextNum = '';
 let operator = '';
 let displayValue = '';
+let numberFlag = true;
 let result = new operate;
 
 const numberButtons = document.querySelectorAll(".numBtn");
@@ -11,12 +12,24 @@ const equalsButton = document.querySelector(".equals-button");
 const buttons = document.querySelectorAll("button");
 const display = document.querySelector(".display")
 
-buttons.forEach(btn => {
+numberButtons.forEach(btn => {
     btn.addEventListener('click', () => {
         console.log(btn.innerHTML);
         updateDisplay(btn);
-        console.log(displayValue);
-        console.log(splitValue);
+        console.log(`Display Value: ${displayValue}`);
+        console.log(`First Number: ${firstNum}`)
+        console.log(`Next Number: ${nextNum}`)
+        console.log(`Operator: ${operator}`)
+    })
+})
+
+operatorButtons.forEach(btn => {
+    btn.addEventListener('click', () => {
+        operator = btn.innerHTML;
+        console.log(`Display Value: ${displayValue}`);
+        console.log(`First Number: ${firstNum}`)
+        console.log(`Next Number: ${nextNum}`)
+        console.log(`Operator: ${operator}`)
     })
 })
 
